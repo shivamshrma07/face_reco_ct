01cd7f6a05680150443a2bcef99f55d3f5804e36
Shivam Sharma <sharmash2000@gmail.com>
2021-03-29 19:13:53 +0530

    Update fr_utils.py

--- fr_utils.py
+++ fr_utils.py
@@ -184,7 +184,6 @@ def load_dataset():
 
 def img_to_encoding(image_path, model):
     img1 = cv2.imread(image_path, 1)
-    img1 = cv2.resize(img1,(96,96))
     img = img1[...,::-1]
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
